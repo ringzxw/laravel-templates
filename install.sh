e26b14ffd73d8526caba7663a3cf00da182ad164
ringzxw <119828376@qq.com>
2019-06-11 12:40:38 +0800

    初始化

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-php artisan vendor:publish --provider="Barryvdh\Debugbar\ServiceProvider"
+cd database/migrations
+shopt -s extglob
+rm -rf !(2014_10_12_000000_create_users_table.php|2014_10_12_100000_create_password_resets_table.php)
+shopt -u extglob
+cd ..
+cd ..
+rm -rf app/Admin
+rm -rf public/vendor
 php artisan vendor:publish --provider="Laravel\Horizon\HorizonServiceProvider"
 php artisan vendor:publish --provider="Encore\Admin\AdminServiceProvider"
 php artisan admin:install
