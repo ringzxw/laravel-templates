8df339cd066e7fb51a3a4cb0bcaddfd6fff61542
ringzxw <119828376@qq.com>
2019-06-11 17:07:03 +0800

    初始化脚本

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+composer update
 cd database/migrations
 shopt -s extglob
 rm -rf !(2014_10_12_000000_create_users_table.php|2014_10_12_100000_create_password_resets_table.php)
@@ -12,6 +13,7 @@ php artisan vendor:publish --provider="Encore\Admin\AdminServiceProvider"
 php artisan admin:install
 php artisan queue:failed-table
 php artisan migrate
+php artisan jwt:secret --force
 php artisan ide-helper:generate
 php artisan ide-helper:meta
-php artisan ide-helper:models
+php artisan ide-helper:models -no
