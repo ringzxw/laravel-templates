31e0af4e6d26686a164c6b94f0c581161a8e7e79
ringzxw <119828376@qq.com>
2019-06-11 12:03:00 +0800

    初始化

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
 php artisan vendor:publish --provider="Barryvdh\Debugbar\ServiceProvider"
+php artisan vendor:publish --provider="Laravel\Horizon\HorizonServiceProvider"
+php artisan vendor:publish --provider="Encore\Admin\AdminServiceProvider"
+php artisan admin:install
+php artisan queue:failed-table
+php artisan migrate
+php artisan ide-helper:generate
+php artisan ide-helper:meta
+php artisan ide-helper:models
